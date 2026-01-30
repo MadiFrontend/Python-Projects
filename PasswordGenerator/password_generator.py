f31602ffacd533684625f366fe2aae98b55f174c
madidev <madifrontend@gmail.com>
2026-01-30 11:30:00 +0330

    feat: add password strength evaluation function

--- PasswordGenerator/password_generator.py
+++ PasswordGenerator/password_generator.py
@@ -30,3 +30,36 @@ def generate_password(length=12, use_digits=True, use_special_chars=True):
     random.shuffle(password_chars)
 
     return "".join(password_chars)
+
+
+def calculate_password_strength(password):
+    """
+    Evaluate password strength based on various criteria
+    """
+    strength = 0
+
+    if len(password) >= 8:
+        strength += 1
+    if len(password) >= 12:
+        strength += 1
+    if len(password) >= 16:
+        strength += 1
+
+    if any(char.isdigit() for char in password):
+        strength += 1
+
+    if any(char.islower() for char in password) and any(
+        char.isupper() for char in password
+    ):
+        strength += 1
+
+    special_chars = "!@#$%^&*()_-+=<>?"
+    if any(char in special_chars for char in password):
+        strength += 1
+
+    if strength <= 2:
+        return "Weak"
+    elif strength <= 4:
+        return "Medium"
+    else:
+        return "Strong"
